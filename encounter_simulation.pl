14cf8bc7805029694a0e23647a3a944f561bb6dc
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2019-11-15 18:08:55 +0700

    Added comments and remove unused files

--- encounter_simulation.pl
+++ encounter_simulation.pl
@@ -3,6 +3,10 @@
 
 :- include('pokemon.pl').
 
+/* Encounter pokemon Simulation */
+/* Process : Generates two random numbers, the Pokemon that will be encountered and a random weight value*/
+/*           If the random weight value is smaller than the weight, the pokemon will be encountered. */
+
 encounter_pokemon(IdLimit,Pokemon):-
     random(0,101,EncounterWeight),
     random(1,IdLimit,EncounterId),
