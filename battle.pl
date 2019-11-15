7d081f15b5929f89c131ff83e248572168fe272f
ozer0532itb <ozer0532@students.itb.ac.id>
2019-11-16 03:46:15 +0700

    Update battle (unfinished) ~zer

---
+++ battle.pl
@@ -0,0 +1,65 @@
+:- include('pokemon.pl').
+:- include('player_status.pl').
+:- include('pokemon_status.pl').
+
+% selected_pokemon(IdSlot)
+:- dynamic(selected_pokemon/1).
+
+:- dynamic(enemy_pokemon/1).
+
+:- dynamic(enemy_health/1).
+
+% Ada bila player sedang battle
+:- dynamic(in_battle/0).
+
+% Inisialisasi selected_pokemon
+selected_pokemon(0).
+
+% attack_multiplier(AttackType, DefendType, Multiplier)
+attack_multiplier(fire, fire, 1).
+attack_multiplier(fire, water, 0.5).
+attack_multiplier(fire, grass, 1.5).
+attack_multiplier(water, fire, 1.5).
+attack_multiplier(water, water, 1).
+attack_multiplier(water, grass, 0.5).
+attack_multiplier(grass, fire, 0.5).
+attack_multiplier(grass, water, 1.5).
+attack_multiplier(grass, grass, 1).
+
+calc_attack(AttackerId, DefenderId, Result) :-
+    type(AttackerId, AtkType),
+    type(DefenderId, DefType),
+    attack(AttackerId, AtkVal),
+    attack_multiplier(AtkType, DefType, Mult),
+    Res is (AtkVal * Mult),
+    Result is round(Res).
+
+% Success Condition: Mengubah selected_pokemon menjadi X
+pick(X) :-
+    in_battle,
+    X > 0,
+    X =< 6, !, 
+    retract(selected_pokemon(AnyOne)),
+    assertz(selected_pokemon(X)).
+
+% Fail Condition: Tidak dalam battle
+pick(X) :-
+    (\+ in_battle), !,
+    write('Wrong command jancug...'), nl,
+    write('You are currently not in a battle').
+
+% Fail Condition: Angka slot tidak benar
+pick(X) :-
+    !,
+    write('Wrong parameter jancug...'), nl,
+    write('Usage: pick(1..6)').
+
+
+attack :-
+    in_battle.
+
+
+% Player sudah memilih untuk bertarung/lari
+% Player juga sudah memilih pokemon pertama untuk dimainkan 
+battle_loop :-
+    !.
