3ce032e913a52908f61533978cb772e90933ee72
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2019-11-15 18:20:59 +0700

    finalizing encounter algorithm

--- encounter_simulation.pl
+++ encounter_simulation.pl
@@ -4,16 +4,19 @@
 :- include('pokemon.pl').
 
 /* Encounter pokemon Simulation */
-/* Process : Generates two random numbers, the Pokemon that will be encountered and a random weight value*/
+/* Process : Generates three random numbers, check if encounter possible. the Pokemon that will be encountered and a random weight value*/
 /*           If the random weight value is smaller than the weight, the pokemon will be encountered. */
 
 encounter_pokemon(IdLimit,Pokemon):-
+    random(0,101,IsEncounter),
+    IsEncounter =< 50,
     random(0,101,EncounterWeight),
     random(1,IdLimit,EncounterId),
     encounter_weight(EncounterId,EncounterRate),
     Pokemon is EncounterId,
     EncounterWeight=<EncounterRate,
-    write("You encountered a/an "),
+    write("You encountered a "),
     poke_name(EncounterId, EncounterName),
     write(EncounterName),
     write("!\n").
+
