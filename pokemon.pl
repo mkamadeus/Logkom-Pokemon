12c6955587beb8a57ad5ec3bd6b836a58170d89c
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2019-11-15 21:17:25 +0700

    Fixed comment style on pokemon.pl

--- pokemon.pl
+++ pokemon.pl
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 /* File : pokemon.pl */
 /* Place to store Pokemon Facts */
 
-% ID and Name
+/* ID and Name */
 poke_name(1, "Bulbasaur").
 poke_name(2, "Charmander").
 poke_name(3, "Squirtle").
@@ -15,7 +14,7 @@ poke_name(9, "Popplio").
 poke_name(10, "Professor Oak").
 poke_name(11, "Tuan Yon").
 
-% Encounter Weight
+/* Encounter Weights */
 encounter_weight(1, 90).
 encounter_weight(2, 90).
 encounter_weight(3, 90).
@@ -28,7 +27,7 @@ encounter_weight(9, 50).
 encounter_weight(10, 10).
 encounter_weight(11, 5).
 
-% Pokemon Type
+/* Pokemon Type */
 type(1, grass).
 type(2, fire).
 type(3, water).
@@ -41,7 +40,7 @@ type(9, water).
 type(10, grass).
 type(11, fire).
 
-% Base health
+/* Pokemon Health */
 health(1, 45).
 health(2, 39).
 health(3, 44).
@@ -54,7 +53,7 @@ health(9, 50).
 health(10, 100).
 health(11, 100).
 
-% Base attack
+/* Pokemon Attack */
 attack(1, 45).
 attack(2, 39).
 attack(3, 44).
@@ -66,72 +65,3 @@ attack(8, 45).
 attack(9, 50).
 attack(10, 100).
 attack(11, 100).
-=======
-/* File : pokemon.pl */
-/* Place to store Pokemon Facts */
-
-/* ID and Name */
-poke_name(1, "Bulbasaur").
-poke_name(4, "Charmander").
-poke_name(7, "Squirtle").
-poke_name(152, "Chikorita").
-poke_name(155, "Cyndaquil").
-poke_name(158, "Totodile").
-poke_name(722, "Rowlet").
-poke_name(725, "Litten").
-poke_name(728, "Popplio").
-poke_name(888, "Professor Oak").
-poke_name(999, "Tuan Yon").
-
-/* Encounter weight */
-encounter_weight(1, 90).
-encounter_weight(4, 90).
-encounter_weight(7, 90).
-encounter_weight(152, 70).
-encounter_weight(155, 70).
-encounter_weight(158, 70).
-encounter_weight(722, 50).
-encounter_weight(725, 50).
-encounter_weight(728, 50).
-encounter_weight(888, 10).
-encounter_weight(999, 5).
-
-/* Pokemon type */
-type(1, grass).
-type(4, fire).
-type(7, water).
-type(152, grass).
-type(155, fire).
-type(158, water).
-type(722, grass).
-type(725, fire).
-type(728, water).
-type(888, grass).
-type(999, fire).
-
-/* Base health */
-health(1, 45).
-health(4, 39).
-health(7, 44).
-health(152, 45).
-health(155, 39).
-health(158, 50).
-health(722, 68).
-health(725, 45).
-health(728, 50).
-health(888, 100).
-health(999, 100).
-
-/* Base attack */
-attack(1, 45).
-attack(4, 39).
-attack(7, 44).
-attack(152, 45).
-attack(155, 39).
-attack(158, 50).
-attack(722, 68).
-attack(725, 45).
-attack(728, 50).
-attack(888, 100).
-attack(999, 100).
->>>>>>> player-status
